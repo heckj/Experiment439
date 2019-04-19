f864d6b00de9c14310b84396c55325a579151244
Joe Heck <heckj@mac.com>
2019-04-19 13:43:52 -0700

    more notes and thinking about how to tackle this

--- Vizcode/Axis.swift
+++ Vizcode/Axis.swift
@@ -9,6 +9,21 @@
 import Foundation
 import SceneKit
 
+/*
+ Axis - a visual representation
+ built on a scale
+ specific to a dimension (x, y, or z)
+ draws the ticks, numbers the line, etc for a side of a chart
+ • axisBottom(scale)
+ • axisTop(scale)
+ • axisLeft(scale)
+ • axisRight(scale)
+ • axis.ticks(count) passes down to scale.domain...
+ • or axis.ticks(d3.timeMinute.every(15)) to create a tick every 15 min
+ • ticks have a size - both inner and outer
+ often has a margin (spacing) and translation from the side it's attached
+*/
+
 public enum AxisDirection {
     case x
     case y
@@ -30,4 +45,27 @@ public struct Axis {
  model changes into the Node hierarchy? For example, does it act like a controller in an MVC kind
  of context?
 
+ if something acts as a controller, we probably want it to be a class (?maybe? not sure of
+ tradeoffs there) and have it keep references to the SCNNodes that it's both rendered and is
+ twiddling/changing. The SCNNode sort of being the moral equivalent of the "view" in a
+ ViewController. That includes removing and add nodes from the hierarchy under which it sets, and
+ I guess "knowing" when it needs to do so based on method calls/data changes.
+
+ So my hypothetical 3d-viz for an equiv of a scatterplot:
+
+ --+ root SCNNode
+   |
+   +- xAxis
+   +- yAxis
+   +- plotPlane
+   +- legend
+
+ viewControllers update what's presented through interaction feedback or when getting new data,
+ and then it does a reloadData or insert/remove|data call - more often reload triggers the dataSource
+ delegate methods, so the dataSource can answer questions about itself as the view needs.
+
+ So maybe we start with computed properties of Axis to generate SCNNode classes on request/demand, but
+ don't keep any references to existing SCNNodes for updating - separating concerns for generating and
+ updating, at least to start...
+ 
  */
