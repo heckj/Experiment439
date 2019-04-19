19a75b3a867c23ddb97ca34dc4c0e1731bd04068
Joe Heck <heckj@mac.com>
2019-04-18 17:15:49 -0700

    Initial Commit

---
+++ Experiment439/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Experiment439
+//
+//  Created by Joseph Heck on 4/18/19.
+//  Copyright © 2019 BackDrop. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Experiment439/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Experiment439
+//
+//  Created by Joseph Heck on 4/18/19.
+//  Copyright © 2019 BackDrop. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
