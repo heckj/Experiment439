a79a165c345bef251ac6cedf6f7c02945acec525
Joe Heck <heckj@mac.com>
2019-04-18 17:34:27 -0700

    twiddlin, but nothing really

--- Experiment439Tests/Experiment439Tests.swift
+++ Experiment439Tests/Experiment439Tests.swift
@@ -12,15 +12,18 @@ import XCTest
 class Experiment439Tests: XCTestCase {
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
 
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(true)
     }
 
     func testPerformanceExample() {
